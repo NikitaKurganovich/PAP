c405126635ebd1e8348008e3f50e864181e7e019
Babananick <babananick@gmail.com>
2023-10-12 21:54:08 +0300

    Update project SDK to API 34. Added google fonts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,15 @@ plugins {
 
 android {
     namespace = "com.example.papproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.papproject"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        compileSdkPreview = "UpsideDownCakePrivacySandbox"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -47,10 +48,10 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
--- app/src/main/java/com/example/papproject/screens/HomeScreen.kt
+++ app/src/main/java/com/example/papproject/screens/HomeScreen.kt
@@ -2,23 +2,60 @@ package com.example.papproject.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.googlefonts.Font
+
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.example.papproject.R
 
 class HomeScreen: Screen {
     @Composable
     override fun Content() {
+        val provider = GoogleFont.Provider(
+            providerAuthority = "com.google.android.gms.fonts",
+            providerPackage = "com.google.android.gms",
+            certificates = R.array.com_google_android_gms_fonts_certs
+        )
+
         val navigator = LocalNavigator.currentOrThrow
-        Column(Modifier.fillMaxSize()) {
+        Column(Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
             Text("WELCOME TO PAP!!!!!!",
-                style = MaterialTheme.typography.displayLarge)
+                Modifier
+                .width(274.dp)
+                .height(50.dp),
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(
+                        Font(googleFont = GoogleFont("Montserrat"), fontProvider = provider)
+                    ),
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF000000),
+                    textAlign = TextAlign.Center,
+                )
+                )
             Text("Do you want leave it?",
+                Modifier
+                    .width(146.dp)
+                    .height(50.dp),
                 style = MaterialTheme.typography.bodySmall)
             Button(
                 onClick = {
